26fd2ed5bb82de8661b4828457c4011953c3106b
Юлия Самсонова <samsonovajulias@gmail.com>
2024-08-09 19:20:31 +0300

    Step 6

--- games/games/prime.py
+++ games/games/prime.py
@@ -2,11 +2,14 @@ import random
 
 
 def is_prime(num):
-    pass
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
 
 
 def prime_game():
     number = random.randint(0, 1000)
     question = f'Question: {number}'
-    correct_answer = ['yes', 'no'][is_prime(number)]
+    correct_answer = ['no', 'yes'][is_prime(number)]
     return question, correct_answer
